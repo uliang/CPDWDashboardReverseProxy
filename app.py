5f4a31dcef962d76b68156505ef66033d672ddc5
uliang <uliang6482@gmail.com>
2019-08-09 21:42:08 +0800

    simplified routing

--- app.py
+++ app.py
@@ -11,15 +11,22 @@ app.config['CAT_APP'] = os.path.join(app.static_folder, 'cat', 'build')
 app.config['DOG_APP'] = os.path.join(app.static_folder, 'dog', 'build')
 
 
+@app.route('/static/<path:filename>')
 @app.route('/<filename>')
 @app.route('/')
 def index(filename=' '):
-    if any([ext in filename for ext in ['json', 'jpeg']]):
+    if any([ext in filename for ext in ['json', 'jpeg', 'js', 'css']]):
         key = session.get('KEY')
         try:
-            return send_from_directory(app.config[key], filename, as_attachment=False)
-        except:
+            filename_parts = filename.split('/')
+            stem = filename_parts[-1]
+            app_static_dir = os.path.join(
+                app.config[key], 'static',  *filename_parts[:-1])
+            return send_from_directory(app_static_dir, stem, as_attachment=False)
+        except KeyError:
             return '', 404
+        except:
+            return send_from_directory(app.config[key], filename, as_attachment=False)
     return render_template('index.html')
 
 
@@ -31,13 +38,13 @@ def choose_pet(pet):
     # return pet
 
 
-@app.route('/static/<path:filename>')
-def static_files(filename):
-    selected_key = session.get('KEY')
-    # print('im here')
-    filename_parts = filename.split('/')
-    stem = filename_parts[-1]
-    app_static_dir = os.path.join(
-        app.config[selected_key], 'static', *filename_parts[:-1])
-    # print(app_static_dir)
-    return send_from_directory(app_static_dir, stem, as_attachment=False)
+# @app.route('/static/<path:filename>')
+# def static_files(filename):
+#     selected_key = session.get('KEY')
+#     # print('im here')
+#     filename_parts = filename.split('/')
+#     stem = filename_parts[-1]
+#     app_static_dir = os.path.join(
+#         app.config[selected_key], 'static', *filename_parts[:-1])
+#     # print(app_static_dir)
+#     return send_from_directory(app_static_dir, stem, as_attachment=False)
